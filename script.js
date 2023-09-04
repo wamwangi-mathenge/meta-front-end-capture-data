c61516e98e29ff563d13527f12defd2a6d5a81ca
Brian <wamwangimathenge@gmail.com>
2023-09-04 11:56:34 +0300

    Write a click-handling function

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ var arr = [
     'Second Click',
     'Third Click'
 ]
+
+function handleClicks() {
+    switch(h1.innerText) {
+        case arr[0]:
+            h1.innerText = arr[1]
+            break
+        case arr[1]:
+            h1.innerText = arr[2]
+            break
+        case arr[2]:
+            h1.innerText = arr[3]
+            break
+        default:
+            h1.innerText = arr[0]
+    }
+}
